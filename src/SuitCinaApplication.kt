edca250de0c8fabfd8a9e8b9966677b7d3ec7233
Robby Suliantoro <robby.suliantoro@gmail.com>
2021-09-11 19:20:17 +0700

    looping

---
+++ src/SuitCinaApplication.kt
@@ -0,0 +1,121 @@
+import elements.Paper
+import elements.Rock
+import elements.Scissors
+import elements.parent.Suit
+import helper.StringContainer
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    startSuitWithCom()
+}
+
+private fun startSuitWithCom(): Boolean {
+    val paper = Paper("paper")
+    val rock = Rock("rock")
+    val scissors = Scissors("scissors")
+
+    val resultList: List<Suit> = listOf(paper, rock, scissors)
+    val compSuit = resultList.random()
+
+    var input: String?
+
+    var hasil: Boolean = true
+
+    //var hasil: Boolean = true
+
+    println("computer milih -> ${compSuit.name}")
+
+    // TODO: 07/09/21 "Tugas buat hari kemis, 9/9/21" -> solved this line (use do-while)
+    /**
+     * Temen2 ngerjakan loop ketika user input selain gunting, batu, kertas
+     * User input selain gunting, batu, kertas -> fungsi redline s/d success -> di loop (diulangin)
+     * User disuruh input lagi
+     * */
+    do {
+        println("input suit (kertas, gunting, batu)")
+        input = readLine()
+//        when(input) {
+//            "batu" -> hasil = true
+//            "kertas" -> hasil = true
+//            "gunting" -> hasil = true
+//            else -> println("silakan masukkan batu/gunting/kertas")
+//        }
+
+//        val mySuit = when (input) {
+//            "gunting" -> Scissors(StringContainer.scissors)
+//            "batu" -> Rock(StringContainer.rock)
+//            else -> Paper(StringContainer.paper)
+//        }
+//
+//        val result = mySuit.actionAgainst(compSuit)
+//        println("you is ${result.status} | you: ${mySuit.name} vs comp: ${compSuit.name}")
+//
+//        println("main lagi? (ketik yes jika pengen)")
+//        val yes = readLine()
+//        if (yes == "yes") {
+//            startSuitWithCom()
+//        } else {
+//            break
+//        }
+//        println(input)
+    } while (input != "gunting" && input != "batu" && input != "kertas")
+
+            val mySuit = when (input) {
+            "gunting" -> Scissors(StringContainer.scissors)
+            "batu" -> Rock(StringContainer.rock)
+            else -> Paper(StringContainer.paper)
+        }
+
+        val result = mySuit.actionAgainst(compSuit)
+        println("you is ${result.status} | you: ${mySuit.name} vs comp: ${compSuit.name}")
+
+    do {
+        println("")
+        println("Apakah Anda masih mau bermain ??? (Pilih ya/tidak)")
+        val readable = readLine().toString().lowercase()
+        when (readable) {
+            "ya" -> startSuitWithCom()
+            "tidak" -> hasil = false
+            else -> println("Pilihan yang Anda masukkan keliru. Pilih ya/tidak.")
+        }
+    } while(readable !="ya" && readable !="tidak")
+
+    return hasil
+}
+
+private fun startSuit() {
+    println("player 1: silahkan input")
+    val input1 = readLine()
+    val suit1 = when (input1) {
+        "gunting" -> Scissors(StringContainer.scissors)
+        "batu" -> Rock(StringContainer.rock)
+        "kertas" -> Paper(StringContainer.paper)
+        else -> Suit("nothing else")
+    }
+
+    if (suit1.name != "nothing else") {
+        println("player 2: silahkan input")
+        val input2 = readLine()
+        val suit2 = when (input2) {
+            "gunting" -> Scissors(StringContainer.scissors)
+            "batu" -> Rock(StringContainer.rock)
+            "kertas" -> Paper(StringContainer.paper)
+            else -> Suit("nothing else")
+        }
+
+        if (suit2.name != "nothing else") {
+            val resultSuit1 = suit1.actionAgainst(suit2)
+            val resultSuit2 = suit2.actionAgainst(suit1)
+
+            println("player1 = ${resultSuit1.status}")
+            println("player2 = ${resultSuit2.status}")
+        } else {
+            println("silahkan input kembali")
+            startSuit()
+        }
+
+    } else {
+        println("silahkan input kembali")
+        startSuit()
+    }
+}
